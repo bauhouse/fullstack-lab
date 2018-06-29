5cf83894541241c25fada2b21e81789f9d02db0d
Stephen Bau <bauhouse@gmail.com>
2018-06-28 22:33:25 -0700

    Adjust white space of class functions

--- cryptex/script.js
+++ cryptex/script.js
@@ -652,31 +652,27 @@ function stepRotation(objRotation) {
 // Class functions from Webkit
 // https://webkit.org/blog-files/3d-transforms/morphing-cubes.html
 
-function hasClassName(inElement, inClassName)
-{
-    var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
-    return regExp.test(inElement.className);
+function hasClassName(inElement, inClassName) {
+  var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
+  return regExp.test(inElement.className);
 }
 
-function addClassName(inElement, inClassName)
-{
-    if (!hasClassName(inElement, inClassName))
-        inElement.className = [inElement.className, inClassName].join(' ');
+function addClassName(inElement, inClassName) {
+  if (!hasClassName(inElement, inClassName))
+    inElement.className = [inElement.className, inClassName].join(' ');
 }
 
-function removeClassName(inElement, inClassName)
-{
-    if (hasClassName(inElement, inClassName)) {
-        var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
-        var curClasses = inElement.className;
-        inElement.className = curClasses.replace(regExp, ' ');
-    }
+function removeClassName(inElement, inClassName) {
+  if (hasClassName(inElement, inClassName)) {
+    var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
+    var curClasses = inElement.className;
+    inElement.className = curClasses.replace(regExp, ' ');
+  }
 }
 
-function toggleClassName(inElement, inClassName)
-{
-    if (hasClassName(inElement, inClassName))
-        removeClassName(inElement, inClassName);
-    else
-        addClassName(inElement, inClassName);
+function toggleClassName(inElement, inClassName) {
+  if (hasClassName(inElement, inClassName))
+    removeClassName(inElement, inClassName);
+  else
+    addClassName(inElement, inClassName);
 }
