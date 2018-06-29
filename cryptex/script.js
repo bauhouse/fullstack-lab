c004f3051b54f1508a684d350eac46f3c28f4cf5
Stephen Bau <bauhouse@gmail.com>
2018-06-28 17:49:35 -0700

    Reformat white space for strings array

--- cryptex/script.js
+++ cryptex/script.js
@@ -120,10 +120,12 @@ function initialize() {
 function initializeRings() {
 
   // Characters: A-Z, 0-9
-  strings = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+  strings = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I",
     "J", "K", "L", "M", "N", "O", "P", "Q", "R",
     "S", "T", "U", "V", "W", "X", "Y", "Z", "0",
-    "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    "1", "2", "3", "4", "5", "6", "7", "8", "9"
+  ];
 
   // Numbers dictionary
   numbers = {
